70bd005c8b8781b2ebf6867ec523d2ce0a5fd01c
Ishan Nag <nagishan710@gmail.com>
2023-06-13 16:59:02 +0530

    list commit

---
+++ Jdbc_ExceptionHandling/src/com/movie/LesserProductionCostException.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+public class LesserProductionCostException extends RuntimeException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LesserProductionCostException(String message) {
+        super(message);
+    }
+}
+
---
+++ Jdbc_ExceptionHandling/src/com/movie/ShorterMovieNameException.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+public class ShorterMovieNameException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShorterMovieNameException(String message) {
+        super(message);
+    }
+}
+
